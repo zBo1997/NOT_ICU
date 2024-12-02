47a4eef5f15718797f860384b694244923fa5d16
zhangbohan <zhangbohan@vv.cn>
2024-12-02 14:46:37 +0800

    feat: supports enter to send messages

--- src/components/chat-com.tsx
+++ src/components/chat-com.tsx
@@ -72,7 +72,6 @@ export function ChatCard() {
           enterKeyHint="send"
           onKeyDown={(event) => event.key === "Enter" && sendMessage()}
         />
-        <Button onClick={sendMessage}>发送</Button>
       </CardFooter>
     </Card>
   );
