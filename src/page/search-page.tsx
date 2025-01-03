890e386acd88c49a89454ee2a13b456a58bb0eb3
zBo1997 <605345115@qq.com>
2025-01-03 19:59:13 +0800

    Update search-page.tsx
    格式化

--- src/page/search-page.tsx
+++ src/page/search-page.tsx
@@ -4,9 +4,8 @@ export function SearchPage() {
     return (
         
             <div className="p-10">
-            <div className="text-2xl font-bold tracking-tight p-10">
-                {/* 这里打算留出面包屑功能 */}
-            </div>
+                <div className="text-2xl font-bold tracking-tight p-10">
+                </div>
                 {/* 插入 PaginatedAccordion */}
                 <TableCom />
             </div>
