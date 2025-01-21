c2542045dd3a14a8c6aa0a2cf9d2a66a93f2751d
MoMo <zhubo@vv.cn>
2025-01-21 17:52:13 +0800

    picture
    picture

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -104,7 +104,7 @@ export function LoginForm({
           </form>
           <div className="relative hidden bg-muted md:block">
             <img
-              src="src\assets\home_picture.png"
+              src="/home_picture.png"
               alt="Image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
