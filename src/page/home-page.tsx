528ccbd1db5381fb67c51a5044b1aca16492f5c6
MoMo <zhubo@vv.cn>
2025-02-06 10:40:49 +0800

    actions

--- src/page/home-page.tsx
+++ src/page/home-page.tsx
@@ -3,7 +3,7 @@ import { PaginatedAccordion } from "@/components/paginated-accordion"; // 导入
 export function HomePage() {
     return (
       <div className="p-10">
-        <div className="text-3xl font-semibold tracking-tight mb-6">热榜</div>
+        <div className="text-3xl font-semibold tracking-tight mb-6">热 榜</div>
         {/* 插入 PaginatedAccordion */}
         <PaginatedAccordion />
       </div>
