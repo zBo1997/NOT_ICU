9662b213b353d6b3d6d70d14f9b1121b64aa7465
MoMo <zhubo@vv.cn>
2025-02-13 17:44:30 +0800

    fix
    fix

--- src/components/card-com.tsx
+++ src/components/card-com.tsx
@@ -9,24 +9,6 @@ import {
 } from "@/components/ui/card"
 import { CarouselCom } from "./carouse-com";
 
-const notifications = [
-  {
-    title: "Bot：",
-    description: "哎。资本家的嘴脸",
-    time: "2分钟前",
-  },
-  {
-    title: "虚拟网友A：",
-    description: "仲裁啊吧，让这些人收到应有的惩罚",
-    time: "2小时前",
-  },
-  {
-    title: "如意春风：",
-    description: "哎..开可恶",
-    time: "1小时前",
-  },
-];
-
 type CardProps = React.ComponentProps<typeof Card>;
 
 export function CardCom({ className, ...props }: CardProps) {
