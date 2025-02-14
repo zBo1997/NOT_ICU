e8af0ce27f676a6978c919a084ff308ccc22be9b
zBo1997 <605345115@qq.com>
2025-02-14 23:12:21 +0800

    modify info card.tsx

--- src/components/info-card-com.tsx
+++ src/components/info-card-com.tsx
@@ -2,7 +2,8 @@ import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { User } from "@/common/user-context";
 import { AvatarUploadCom } from "@/components/avatar-upload";
-
+import { useAlert } from "@/context/alert-context"; // 导入 useAlert
+import { post } from "@/utils/request"; // 引入刚刚写的请求工具类
 export function InfoCardCom({
   user,
   children,
@@ -10,6 +11,7 @@ export function InfoCardCom({
   user: User | null;
   children: React.ReactNode;
 }) {
+  const { showAlert } = useAlert(); // 获取 showAlert 方法
   return (
     <Card>
       <CardContent className="rounded-xl p-16 space-y-6">
@@ -22,10 +24,33 @@ export function InfoCardCom({
                 userName: user?.name || "",
                 avatarUrl: user?.avatar || "",
               }}
-              onAvatarChange={(newAvatarUrl) => {
+              onAvatarChange={async (newAvatarUrl) => {
                 // 更新用户信息
                 // 这里只是模拟更新用户信息，实际项目中需要调用接口
                 user!.avatar = newAvatarUrl;
+                // 更新用户头像
+                try {
+                  const response = await post<{
+                    error: string;
+                    errorContent: string;
+                    token: string;
+                  }>("updateUser", {
+                    user,
+                  });
+                  if (response.data.error) {
+                    showAlert(response.data.error, response.data.errorContent); // 更新失败弹出警告
+                    return;
+                  } else {
+                    //设置用户信息
+                    localStorage.setItem("user", JSON.stringify(response.data)); // 保存用户信息
+                    // 如果更新成功，可以保存 token 或用户信息到本地
+                    window.location.reload(); // 重新加载页面
+                  }
+                } catch (error) {
+                  showAlert("更新失败", "服务器出小差了"); // 更新失败弹出警告
+                  console.log(error);
+                  return;
+                }
               }}
               size="lg"
               className="mt-1"
