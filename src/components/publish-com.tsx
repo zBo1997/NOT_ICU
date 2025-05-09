f24c246d53a0258f891a31d04da11a1f8982b67a
MoMo <zhubo@vv.cn>
2025-05-09 17:27:03 +0800

    Update publish-com.tsx

--- src/components/publish-com.tsx
+++ src/components/publish-com.tsx
@@ -12,12 +12,12 @@ export function PublishCom() {
       {/* 头部 */}
       <CardHeader>
         <CardTitle className="text-2xl font-bold text-primary">
-          titile
+          敬请期待、暂未开放发布功能
         </CardTitle>
       </CardHeader>
 
       {/* 消息内容区域 */}
-      <CardContent>这里是内容区域</CardContent>
+      <CardContent>敬请期待、暂未开放发布功能</CardContent>
 
       {/* 输入框和按钮 */}
       <CardFooter className="flex space-x-2"></CardFooter>
